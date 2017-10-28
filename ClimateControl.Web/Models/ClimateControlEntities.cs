8f86e2dd730aac43d4a5d7f085d8f12b07dff3a1
Andrii Ivanskiy <andrii@ivanskiy.com>
2017-10-28 13:31:39 +0300

    Change database first to code first EF approach

---
+++ ClimateControl.Web/Models/ClimateControlEntities.cs
@@ -0,0 +1,21 @@
+namespace ClimateControl.Web.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ClimateControlEntities : DbContext
+    {
+        public ClimateControlEntities()
+            : base("name=ClimateControlEntities")
+        {
+        }
+
+        public virtual DbSet<SensorData> SensorData { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
