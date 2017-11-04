3fa082b79a400739b59a28db6928f15bfbfea622
Andrii Ivanskiy <andrii@ivanskiy.com>
2017-11-04 13:26:27 +0200

    Fix time

--- ClimateControl.Web/Helpers/TimeZoneConverter.cs
+++ ClimateControl.Web/Helpers/TimeZoneConverter.cs
@@ -8,10 +8,10 @@ namespace ClimateControl.Web.Helpers
 {
     public static class TimeZoneConverter
     {
-        private const string ConvertToTimeZone = "FLE Standard Time";        
+        private const string TimeZone = "FLE Standard Time";        
         public static DateTime Convert(DateTime utcTime)
         {
-            var localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time"));
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, TimeZoneInfo.FindSystemTimeZoneById(TimeZone));
             return localTime;
         }
     }
